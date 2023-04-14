7096efe8e1890e326bf82c5ce375e79db85e3d8a
Eduardo Borges <71708644+borgess-dg@users.noreply.github.com>
2023-04-14 17:46:58 -0300

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private static DataOutputStream dataOutputStream = null;
+    private static DataInputStream dataInputStream = null;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual o caminho do arquivo?");
+        String fileName = scanner.nextLine();
+        scanner.close();
+        try(Socket socket = new Socket("localhost",5000)) {
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            sendFile(fileName);
+            
+            dataInputStream.close();
+            dataInputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendFile(String path) throws Exception{
+        int bytes = 0;
+        File file = new File(path);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        
+        // send file size
+        dataOutputStream.writeLong(file.length());  
+        // break file into chunks
+        byte[] buffer = new byte[4*1024];
+        while ((bytes=fileInputStream.read(buffer))!=-1){
+            dataOutputStream.write(buffer,0,bytes);
+            dataOutputStream.flush();
+        }
+        fileInputStream.close();
+    }
+}
